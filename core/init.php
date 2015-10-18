bd6d969c71efc843eeb15b1e4e4268e22ca9e670
Chamath Silva <mbckchamathsilva@gmai.com>
2015-10-18 16:38:36 +0530

    DB PDO basic

---
+++ core/init.php
@@ -0,0 +1,21 @@
+<?
+$GLOBALS['config'] = array(
+    'mysql' => array(
+        'host' => 'localhost',
+        'user_name' => 'root',
+        'password' => 'root',
+        'db' => 'testdb'
+    )
+);
+
+
+spl_autoload_register(function($classname){
+    $filename ='../classes/' . $classname .'.php';
+    if(file_exists($filename)){
+        require $filename;
+    }
+    else{
+        echo $classname;
+        echo "There is no such file";
+    }
+});
